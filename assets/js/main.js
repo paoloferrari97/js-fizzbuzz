4b4edffb93b16b6eaf963ae13c37f3701f0debd3
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-04-27 15:48:56 +0200

    Stampo a video i numeri da 1 a 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,19 +4,24 @@ e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
 
+var message = "";
 for (var i = 1; i <= 100; i++){
 
     //risolvo l'esercizio con if e else if
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz");
+        message += "FizzBuzz" + ", ";
     } else if (i % 3 == 0) {
         console.log("Fizz");
+        message += "Fizz" + ", ";
     } else if (i % 5 == 0) {
         console.log("Buzz");
+        message += "Buzz" + ", ";
     } else {
         console.log(i);
+        message += i + ", ";
     }
-
+    
     //risolvo l'esercizio con switch e case
     /* switch (true) {
         case i % 3 == 0 && i % 5 == 0:
@@ -29,6 +34,7 @@ for (var i = 1; i <= 100; i++){
             console.log("Buzz");
             break;
         default:
-        console.log(i);
+            console.log(i);
     } */
 }
+document.getElementById("container").innerHTML = message;
